16eaf316a13a63ecd27e9957a903feaddee85d67
sidak <spssachdeva21695@gmail.com>
2015-06-11 14:27:58 -0400

    correct the function export and call to cb()

--- Amazon_ Review_Scraper/reviewScraper.js
+++ Amazon_ Review_Scraper/reviewScraper.js
@@ -1,8 +1,8 @@
 // use jsdom 3.x with nodejs
 var jsdom = require('jsdom');
 var upvotesList=[], totalVotesList=[], ratingList=[];
-
-scrape = function(name, id, ref, page, cb) {
+var exports = module.exports = {};
+exports.scrape = function(name, id, ref, page, cb) {
     // The url, scripts, done keywords are optional 
     // but if you use them enclose them in curly braces 
     jsdom.env({
@@ -23,12 +23,16 @@ scrape = function(name, id, ref, page, cb) {
 						.children('div.a-section.review')
 						.each(function() {
 							//console.log("fuck");
+							var upvotes=0;
+							var totalVotes=0;
 							var helpfulness = $(this).find("div.a-row.helpful-votes-count").find("span.a-size-small.a-color-secondary.review-votes").text();
 							if(helpfulness==""){
 								helpfulness=0;
 							}
-							var upvotes=helpfulness.split(" ")[0];
-							var totalVotes= helpfulness.split(" ")[2];
+							else{
+								upvotes=helpfulness.split(" ")[0];
+								totalVotes= helpfulness.split(" ")[2];
+							}
 							var rating = $(this).find("div.a-row.helpful-votes-count").next()
 										.find("a.a-link-normal").find("i")
 										.find("span.a-icon-alt").text();
@@ -37,19 +41,15 @@ scrape = function(name, id, ref, page, cb) {
 							totalVotesList.push(totalVotes);
 							ratingList.push(rating);
 							
-							console.log("Helpfulness: " + upvotes+"/"+totalVotes  +"\nRating: " + rating +"\n\n");
+							console.log("IN reveiewScraper.js Helpfulness: " + upvotes+"/"+totalVotes  +"\nRating: " + rating +"\n\n");
 						});
 					window.close();
 					var result=[];
 					result.push(upvotesList, totalVotesList, ratingList);
-					cb(result);
+					cb(null,result);
 				}
     });
 
 };
-var exports = module.exports = {};
-
-module.exports=scrape;
-
 
 // USAGE : node amazonScraper.js FIFA-15-PlayStation-4 B00KPY1GJA cm_cr_pr_btm_link_3 3  >page3.txt 
