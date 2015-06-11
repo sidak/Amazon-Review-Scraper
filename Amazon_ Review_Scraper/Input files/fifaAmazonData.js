aeb553b6170cadea0e903c6ab958ecd493ec06fa
sidak <spssachdeva21695@gmail.com>
2015-06-11 00:50:29 -0400

    add fifa data from amazon in json

---
+++ Amazon_ Review_Scraper/Input files/fifaAmazonData.js
@@ -0,0 +1,50 @@
+//cm_cr_pr_btm_link_i
+//i
+var fifa14=[
+			{
+				name:"FIFA-14-Playstation-3",
+				id:"B00CD90R72",
+			},
+			
+			{
+				name:"FIFA-14-PlayStation-4",
+				id:"B00CXCCI8A",
+			},
+			
+			{
+				name:"FIFA-14-Xbox-One",
+				id:"B00CX7FMDS",
+			},
+			
+			{
+				name:"FIFA-14-Xbox-360",
+				id:"B00CD90R4K",
+			},
+		
+		];
+
+var fifa15=[
+			{
+				name:"FIFA-15-PlayStation-3",
+				id:"B00KPY1GX6",
+			},
+			
+			{
+				name:"FIFA-15-PlayStation-4",
+				id:"B00KPY1GJA",
+			},
+			
+			{
+				name:"FIFA-15-Xbox-One",
+				id:"B00KPY1HP8",
+			},
+			
+			{
+				name:"FIFA-15-Xbox-360",
+				id:"B00KPY1GFY",
+			},
+		
+		];
+
+module.exports=fifa14;
+module.exports=fifa15;
