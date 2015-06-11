6c583c3541969789f2704114ad7dd05f881da1e1
sidak <spssachdeva21695@gmail.com>
2015-06-11 18:47:50 -0400

    refactor code, add comments & remove magic numbers

--- Amazon_ Review_Scraper/calcRelevance.js
+++ Amazon_ Review_Scraper/calcRelevance.js
@@ -10,6 +10,9 @@ var KEY_FIFA15="fifa15";
 var KEY_UVLIST="upvotesList";
 var KEY_TVLIST="totalVotesList";
 var KEY_RLIST="ratingList";
+var NUM_VERSIONS=2;
+var NUM_PLATFORMS=4;
+var NUM_PAGES_TO_SCRAPE=2;
 
 var upvotesList=[];
 var totalVotesList=[];
@@ -21,115 +24,90 @@ var relevanceScores=[];
 var games=["fifa14","fifa15"];
 
 
-// Async task (same in all examples in this chapter)
+// Async task for collecting the reviews on a page 
+// numbered as (2-ct)+1 for game and idx as the platform
+
 var collectPageRatingStep= function (ct,game, idx, cb) {
-	var normCt= (2-ct)+1;
+	var normCt= (NUM_PAGES_TO_SCRAPE-ct)+1;
 	var ref=refBase+normCt;
 	var pageNumber=normCt;
 
 	console.log("let's scrap the page num "+pageNumber+"in the game " +game+ "with platform idx "+idx+'\n');
-	console.log(game, " ", idx);
-	console.log(fifaData[game]);
+	//console.log(fifaData[game]);
 	scraper.scrape(fifaData[game][idx][KEY_NAME],fifaData[game][idx][KEY_ID], ref , pageNumber, function(err, result){
 		if(err){
 			console.log("there was an error ", err);
 			cb(err);
 		}
 		else if (result!=null){
-			console.log("before adding the result\n");
-			console.log(fifaData[game][idx]);
+			//console.log("before adding the result\n");
+			//console.log(fifaData[game][idx]);
+			
+			//Concatenate the initial votes list with the one scraped now
 			fifaData[game][idx][KEY_UVLIST]=fifaData[game][idx][KEY_UVLIST].concat(result[0]);
 			fifaData[game][idx][KEY_TVLIST]=fifaData[game][idx][KEY_TVLIST].concat(result[1]);
 			fifaData[game][idx][KEY_RLIST]=fifaData[game][idx][KEY_RLIST].concat(result[2]);
 			console.log("after adding the result\n");
 			console.log(fifaData[game][idx]);
-			console.log('Now going to my cb \n\n');
+			//console.log('Now going to my cb \n\n');
 			cb(null,  result);
 		}
 	});
 
 }
 
-// Final task (same in all the examples)
+// Final task 
 function final(cb) { 
 	cb(null, 'Done scraping the number of elements you said ');
 	
 }
 
-
-function asyncTraversal(ct, game,idx2, traversalStep, cb) {
+// Async traversal of given "ct" of pages for "game" 
+// with platform as "idx" and scrape the most helpful reviews
+// on these pages
+function asyncTraversal(ct, game,idx, traversalStep, cb) {
   if(ct>0) {
-    traversalStep( ct, game, idx2, function(err, result) {
-      //results.push(result);
+    traversalStep( ct, game, idx, function(err, result) {
       if(err)cb(err);
       else {
 		  ct--;
 		  console.log(result)
-		  return asyncTraversal(ct, game, idx2, traversalStep, cb);
+		  return asyncTraversal(ct, game, idx, traversalStep, cb);
 	  }
     });
   } else {
     return final(cb);
   }
 }
-/*
-function collectPlatformRatingStep(game){
-		
-		game = fifaData[KEY_FIFA14][i];
-		idx =i;
-		elements=[1,2];
-		collectRatingForGame(elements.shift());
-		
+// Collect ratings for all versions
+// of the game and for the different platforms it runs on
+function collectGameRatings(cb){
 	
-	}
-	for(var i=0; i<4; i++){
-		
-		game = fifaData[KEY_FIFA14][i];
-		idx =i;
-		elements=[1,2];
-		collectRatingForGame(elements.shift());
-		
-	}
-	
-}
-
-function collectGameRatingStep(){
-	elements=[KEY_FIFA14,KEY_FIFA15];
-	asyncTraversal(elements.shift(), collectPlatformRatingStep, function(err, result){
-		if(err)console.log(err);
-		else if (result!=null){
-			console.log(result);
-		}
-	});
-}
-*/
-function main(cb){
-	console.log(fifaData);
-	for(var i=0; i<2; i++){
+	// There are 2 versions that we are considering
+	// Fifa14 and Fifa15
+	for(var i=0; i<NUM_VERSIONS; i++){
 		(function(){
 			var game=games[i];
-			for(var j=0; j<4; j++){
-				(function(){
-					var idx=j;
-					asyncTraversal(2,game, idx, collectPageRatingStep,function(err, result){
-						if(err)cb(err);
-						else if (result!=null){
-							cb(null,result);
-						}
-					});
-				})();
-			}
+			collectPlatformRatings (game, function(err, result){
+				if(err)cb(err);
+				else if (result!=null){
+					cb(null, result);
+				}
+			});
+						
 		})();
 	}
 }
 
-
-function main1(cb){
-	//var game=;
-	for(var j=0; j<2; j++){
+// Collect ratings for the different platforms
+// for the given game
+function collectPlatformRatings(game, cb){
+	for(var j=0; j<NUM_PLATFORMS; j++){
 		(function(){
-			var idx1=j;
-			asyncTraversal(2,KEY_FIFA14, idx1, collectPageRatingStep,function(err, result){
+			var idx=j;
+			// Collect reviews on only 2 pages for 
+			// given game and platform as idx 
+			asyncTraversal(NUM_PAGES_TO_SCRAPE,game, idx, collectPageRatingStep,function(err, result){
 				if(err)cb(err);
 				else if (result!=null){
 					cb(null,result);
@@ -139,22 +117,7 @@ function main1(cb){
 	}
 }
 
-
-/*
-asyncTraversal(2,KEY_FIFA14,0,collectPageRatingStep,function(err, result){
-	console.log("hey knwf");
-	
-	if(err)console.log(err);
-	else if (result!=null){
-		console.log("done calling");
-		console.log(result);
-		console.log(fifaData[KEY_FIFA14]);
-	}
-	
-});
-*/
-
-main(function(err, result){
+collectGameRatings(function(err, result){
 	if(err)console.log(err);
 	else if (result!=null){
 		console.log(result);
@@ -165,11 +128,5 @@ main(function(err, result){
 });
 
 
-// loop over the whole 
-//call a method for fifa14
-// call a method for fifa15
-
-// calling a method for each calls 4 methods for different platforms
-// store the relevance data in a file 
 // then do the routing part
 // aggregate feedback from start
