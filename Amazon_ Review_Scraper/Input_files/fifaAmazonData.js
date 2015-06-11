05506985df3fa33628d478aa59f96b0e217354a6
sidak <spssachdeva21695@gmail.com>
2015-06-11 05:31:11 -0400

    add more fields for storing votes

---
+++ Amazon_ Review_Scraper/Input_files/fifaAmazonData.js
@@ -0,0 +1,77 @@
+//cm_cr_pr_btm_link_i
+//i
+var fifaData=
+		{
+		fifa14:[
+					{
+						name:"FIFA-14-Playstation-3",
+						id:"B00CD90R72",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[]
+
+						
+					},
+					
+					{
+						name:"FIFA-14-PlayStation-4",
+						id:"B00CXCCI8A",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[]
+					},
+					
+					{
+						name:"FIFA-14-Xbox-One",
+						id:"B00CX7FMDS",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[]
+					},
+					
+					{
+						name:"FIFA-14-Xbox-360",
+						id:"B00CD90R4K",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[]
+					},
+				],
+		
+		
+			fifa15:[
+						{
+							name:"FIFA-15-PlayStation-3",
+							id:"B00KPY1GX6",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[]
+						},
+						
+						{
+							name:"FIFA-15-PlayStation-4",
+							id:"B00KPY1GJA",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[]
+						},
+						
+						{
+							name:"FIFA-15-Xbox-One",
+							id:"B00KPY1HP8",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[]
+						},
+						
+						{
+							name:"FIFA-15-Xbox-360",
+							id:"B00KPY1GFY",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[]
+						}
+			
+			]
+			};
+module.exports=fifaData;
