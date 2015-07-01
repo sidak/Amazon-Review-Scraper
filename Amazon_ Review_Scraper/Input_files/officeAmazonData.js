a84cc376e05294c683d827ff901505e0cdcad8cb
sidak <spssachdeva21695@gmail.com>
2015-07-01 01:32:57 -0400

    add data information for office example

---
+++ Amazon_ Review_Scraper/Input_files/officeAmazonData.js
@@ -0,0 +1,97 @@
+//cm_cr_pr_btm_link_i
+//i
+var officeData=
+		{
+		office2013:[
+					// home and student 2013
+					{
+						name:"Microsoft-Office-Student-1User-Download",
+						id:"B00B1TGUMG",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:187,
+						reviewDatesList:[]
+						
+					},
+					// home & business
+					{
+						name:"Microsoft-Office-Business-1User-Download",
+						id:"B00B1TGMIS",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:80,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"Microsoft-Office-Professional-1User-Download",
+						id:"B00B1TGQCA",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:34,
+						reviewDatesList:[]
+						
+					}
+				],
+		
+		
+			office365:[
+						{
+							name:"Office-Home-Macs-Card-Disc",
+							id:"B009SPTUW2",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:100,
+							reviewDatesList:[]
+						
+						},
+						
+						{
+							name:"Office-365-Personal-Subscription-Card",
+							id:"B00HV9IM58",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:32,
+							reviewDatesList:[]
+						
+						},
+						
+						{
+							name:"Microsoft-Office-Small-Business-Premium",
+							id:"B00AZMREAM",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:7,
+							reviewDatesList:[]
+						
+							
+						},
+						// office 365 university
+						{
+							name:"Microsoft-University-Subscription-Validation-Required",
+							id:"B00B766VZE",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:75,
+							reviewDatesList:[]
+						
+						}
+			
+			]
+			};
+module.exports=officeData;
