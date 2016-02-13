9274c9def9c1387cb33da09762ef172203ffb1c4
sidak <spssachdeva21695@gmail.com>
2016-02-13 15:09:14 +0530

    Add new input files corresponding to required scraping

---
+++ Amazon_ Review_Scraper/Input_files/fifaAmazonData_Feb13.js
@@ -0,0 +1,108 @@
+//cm_cr_pr_btm_link_i
+//i
+var fifaData=
+		{
+		fifa14:[
+					{
+						name:"FIFA-14-Playstation-3",
+						id:"B00CD90R72",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:73,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"FIFA-14-PlayStation-4",
+						id:"B00CXCCI8A",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:65,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"FIFA-14-Xbox-One",
+						id:"B00CX7FMDS",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:25,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"FIFA-14-Xbox-360",
+						id:"B00CD90R4K",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:52,
+						reviewDatesList:[]
+						
+					},
+				],
+		
+		
+			fifa15:[
+						{
+							name:"FIFA-15-PlayStation-3",
+							id:"B00KPY1GX6",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:61,
+							reviewDatesList:[]
+						
+						},
+						
+						{
+							name:"FIFA-15-PlayStation-4",
+							id:"B00KPY1GJA",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:78,
+							reviewDatesList:[]
+						
+						},
+						
+						{
+							name:"FIFA-15-Xbox-One",
+							id:"B00KPY1HP8",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:30,
+							reviewDatesList:[]
+						
+							
+						},
+						
+						{
+							name:"FIFA-15-Xbox-360",
+							id:"B00KPY1GFY",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:50,
+							reviewDatesList:[]
+						
+						}
+			
+			]
+			};
+module.exports=fifaData;
---
+++ Amazon_ Review_Scraper/Input_files/iphoneAmazonData.js
@@ -0,0 +1,61 @@
+//cm_cr_pr_btm_link_i
+//i
+var iphoneData=
+		{
+		iphone5:[
+					{
+						name:"Apple-iPhone-5c-16GB-Pink",
+						id:"B00F3ISWRC",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:96,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"Apple-iPhone-5s-Silver-Unlocked",
+						id:"B00F3J4HCA",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:170,
+						reviewDatesList:[]
+						
+					}
+				],
+		
+		
+			iphone6:[
+						
+						{
+							name:"Apple-iPhone-Gold-16-Unlocked",
+							id:"B00NQGP3SO",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:85,
+							reviewDatesList:[]
+						
+							
+						},
+						
+						{
+							name:"Apple-iPhone-Plus-Gold-Unlocked",
+							id:"B00NQGONB2",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:40,
+							reviewDatesList:[]
+						
+						}
+			
+			]
+			};
+module.exports=iphoneData;
