e7a535896da165b094de0c1ec6e0d1ec58bd5da1
sidak <spssachdeva21695@gmail.com>
2016-02-27 18:32:43 +0530

    Add data format file for scraping reviewer ranking

---
+++ Amazon_Review_Scraper/Input_files/fifaAmazonDataFormat_Feb2016_withRanking.js
@@ -0,0 +1,117 @@
+//cm_cr_pr_btm_link_i
+//i
+var fifaData=
+		{
+		fifa14:[
+					{
+						name:"FIFA-14-Playstation-3",
+						id:"B00CD90R72",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:74,
+						reviewDatesList:[],
+						reviewerRanking:[]
+
+						
+					},
+					
+					{
+						name:"FIFA-14-PlayStation-4",
+						id:"B00CXCCI8A",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:65,
+						reviewDatesList:[],
+						reviewerRanking:[]
+						
+					},
+					
+					{
+						name:"FIFA-14-Xbox-One",
+						id:"B00CX7FMDS",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:25,
+						reviewDatesList:[],
+						reviewerRanking:[]
+						
+					},
+					
+					{
+						name:"FIFA-14-Xbox-360",
+						id:"B00CD90R4K",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:52,
+						reviewDatesList:[],
+						reviewerRanking:[]
+						
+					},
+				],
+		
+		
+			fifa15:[
+						{
+							name:"FIFA-15-PlayStation-3",
+							id:"B00KPY1GX6",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:62,
+							reviewDatesList:[],
+							reviewerRanking:[]
+						
+						},
+						
+						{
+							name:"FIFA-15-PlayStation-4",
+							id:"B00KPY1GJA",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:78,
+							reviewDatesList:[],
+							reviewerRanking:[]
+						
+						},
+						
+						{
+							name:"FIFA-15-Xbox-One",
+							id:"B00KPY1HP8",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:30,
+							reviewDatesList:[],
+							reviewerRanking:[]
+						
+							
+						},
+						
+						{
+							name:"FIFA-15-Xbox-360",
+							id:"B00KPY1GFY",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:50,
+							reviewDatesList:[],
+							reviewerRanking:[]
+						
+						}
+			
+			]
+			};
+module.exports=fifaData;
