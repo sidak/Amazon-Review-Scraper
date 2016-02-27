c6349dc5df6d5eaf87cddab15df416f32c6deda4
sidak <spssachdeva21695@gmail.com>
2016-02-27 07:10:03 +0530

    Organize input files containing format information

---
+++ Amazon_ Review_Scraper/Input_files/iphoneAmazonDataFormat.js
@@ -0,0 +1,134 @@
+//cm_cr_pr_btm_link_i
+//i
+var iphoneData=
+		{
+		iphone5:[
+					{
+						name:"Apple-iPhone-5-Unlocked-Cellphone-Black",
+						id:"B0097CZBH4",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:97,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"Apple-iPhone-5-64GB-White-AT",
+						id:"B0097BXS98",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:12,
+						reviewDatesList:[]
+						
+					},
+
+					{
+						name:"Apple-iPhone-5-Factory-Unlocked-Cellphone",
+						id:"B00ASURUYQ",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:160,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"Apple-iPhone-5s-Unlocked-Cellphone",
+						id:"B00F3J4B5S",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:237,
+						reviewDatesList:[]
+						
+					},
+					
+					{
+						name:"Apple-iPhone-5c-Unlocked-Cellphone",
+						id:"B00F3ISEUC",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:165,
+						reviewDatesList:[]
+						
+					}
+				],
+		
+		
+			iphone6:[
+						
+						{
+							name:"Apple-iPhone-6-Cellular-International-Unlocked",
+							id:"B00NQGP3L6",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:150,
+							reviewDatesList:[]
+						
+							
+						},
+						
+						{
+							name:"Apple-iPhone-6s_plus-Warranty-Unlocked-Cellphone",
+							id:"B015E8VT52",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:15,
+							reviewDatesList:[]
+						
+						},
+
+						{
+							name:"Apple-iPhone-6-Unlocked-Gold-16GB",
+							id:"B00VHSXBUA",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:5,
+							reviewDatesList:[]
+						
+							
+						},
+						
+						{
+							name:"Apple-iPhone-6s-Warranty-Unlocked-Cellphone",
+							id:"B015E8UTIU",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:35,
+							reviewDatesList:[]
+						
+						},
+						
+						{
+							name:"Apple-iPhone-6_plus-Gold-Unlocked",
+							id:"B00NQGOOT8",
+							upvotesList:[],
+							totalVotesList:[],
+							ratingList:[],
+							relevanceList:[],
+							pages:84,
+							reviewDatesList:[]
+						
+						}
+			
+			]
+			};
+module.exports=iphoneData;
