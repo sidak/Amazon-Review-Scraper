fc873a53242dd44e6fdad5245b2dad75fffb1586
sidak <spssachdeva21695@gmail.com>
2016-02-28 17:24:59 +0000

    Add data format file for nba

---
+++ Amazon_Review_Scraper/Input_files/nbaAmazonDataFormat_Feb2016_withReviewerLink.js
@@ -0,0 +1,125 @@
+//cm_cr_pr_btm_link_i
+//i
+var fifaData=
+		{
+		nfl14:[
+					{
+						name:"NBA-2K14-Playstation-3",
+						id:"B00C710AQO",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:52,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+
+						
+					},
+					
+					{
+						name:"NBA-2K14-PlayStation-4",
+						id:"B00DBRM3G8",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:59,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+						
+					},
+					
+					{
+						name:"NBA-2K14-Xbox-360",
+						id:"B00C710AZ0",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:59,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+						
+					},
+					
+					{
+						name:"NBA-2K14-Xbox-One",
+						id:"B00DBRM3EU",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:30,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+						
+					},
+				],
+		
+		
+		nfl15:[
+					{
+						name:"NBA-2K15-PlayStation-3",
+						id:"B00K1JBMDO",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:46,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+					
+					},
+					
+					{
+						name:"NBA-2K15-PlayStation-4",
+						id:"B00K1JBLO4",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:87,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+					
+					},
+					
+					{
+						name:"NBA-2K15-Xbox-One",
+						id:"B00K1JBLNA",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:44,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+					
+						
+					},
+					
+					{
+						name:"NBA-2K15-Xbox-360",
+						id:"B00K1JBMGQ",
+						upvotesList:[],
+						totalVotesList:[],
+						ratingList:[],
+						relevanceList:[],
+						pages:59,
+						reviewDatesList:[],
+						reviewerLink:[],
+						reviewerRanking:[]
+					
+					}
+		
+				]
+		};
+module.exports=fifaData;
