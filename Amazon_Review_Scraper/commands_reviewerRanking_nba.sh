5da8007d21133e3f9f5a1e871311dea6e2e7cf00
sidak <spssachdeva21695@gmail.com>
2016-03-01 01:48:00 +0530

    Add commands to scrape reviewer ranking for nba

---
+++ Amazon_Review_Scraper/commands_reviewerRanking_nba.sh
@@ -0,0 +1,50 @@
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps3_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps3_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps3_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps3_p2.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps3_p3.txt 112 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps3_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps4_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps4_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps4_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps4_p2.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_ps4_p3.txt 185 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_ps4_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_xbox1_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_xbox1_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_xbox1_p2.txt 92 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_xbox1_p2.txt
+
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_xbox360_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_xbox360_p1.txt
+
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_xbox360_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_xbox360_p2.txt
+
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba14_xbox360_p3.txt 183 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba14_xbox360_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps3_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps3_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps3_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps3_p2.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps3_p3.txt 55 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps3_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps4_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps4_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps4_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps4_p2.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps4_p3.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps4_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps4_p4.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps4_p4.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_ps4_p5.txt 62 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_ps4_p5.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_xbox360_p1.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_xbox360_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_xbox360_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_xbox360_p2.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_xbox360_p3.txt 181 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_xbox360_p3.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_xbox1_p1.txt 233 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_xbox1_p1.txt
+
+node reviewerRatingScraper.js Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Links/reviewerLinks_nba15_xbox1_p2.txt 200 > Output\ Files/Review_Scraping_More_Contents/Nba/Reviewer_Ratings/reviewerRating_nba15_xbox1_p2.txt
